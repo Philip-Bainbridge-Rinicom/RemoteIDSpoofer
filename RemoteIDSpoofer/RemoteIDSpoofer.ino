bf04471f252182a1a85b92ddf86907f1ed31dbb5
Philip-Bainbridge-Rinicom <165289000+Philip-Bainbridge-Rinicom@users.noreply.github.com>
2024-06-18 16:06:19 +0100

    Update position from serial port
    Update the centre of the jammers through coordinate updates sent over the UART

--- RemoteIDSpoofer/RemoteIDSpoofer.ino
+++ RemoteIDSpoofer/RemoteIDSpoofer.ino
@@ -2,10 +2,28 @@
 
 #include "frontend.h"
 #include "spoofer.h"
+#include <string.h>
+#include <stdio.h>
 
 // for some reason I can't get vector to work so... this hacky thing for now...
 int num_spoofers = 0;
 Spoofer spoofers[16];
+
+String ReadPos; 
+double Lat;
+double Lon;
+
+char incomingByte = 0;
+String str;
+const byte numChars = 100;
+char receivedChars[numChars];
+char transmitChars[numChars];
+boolean newData = false;
+static boolean recvInProgress = false;
+static byte ndx = 0;
+char startMarker = '$';
+char endMarker = '\r';
+char rc;
 
 void setup() {
   Serial.begin(115200);
@@ -28,10 +46,65 @@ void setup() {
 
 void loop() {
   // do the spoofing
-  
-  for (int i = 0; i < num_spoofers; i++) {
-    spoofers[i].updateLocation(54.063923, -2.811494);
+  /*if (Serial.available() > 0) {
+    ReadPos = Serial.readString();
+    ReadPos.trim();
+      if(ReadPos.startsWith("LAT")){
+        Lat = ReadPos.toCharArray;
+        //Lat == ReadPos;
+        //Serial.print(Lat);
+        //Lat.remove(0, 3);
+        //Serial.print(Lat.toDouble());
+      }
+      else if(ReadPos.startsWith("LON")){
+        //Lon == ReadPos;
+        //Serial.print(Lon);
+        //Serial.print(Lon.toDouble());
+      }
+  }*/
+
+  while (Serial.available() > 0 ) {
+                rc = Serial.read();
+                //Serial.print(rc);
+                if (recvInProgress == true) {
+                        if (rc != endMarker) {
+                                receivedChars[ndx] = rc;
+                                ndx++;
+                                if (ndx >= numChars) {
+                                        ndx = numChars - 1;
+                                }
+                        } else {
+                                receivedChars[ndx] = '\0'; // terminate the string
+                                recvInProgress = false;
+                                ndx = 0;
+                                newData = true;
+                                //Serial.print('.');
+                                //break;
+                        }
+                } else if (rc == startMarker) {
+                        recvInProgress = true;
+                }
+
+                if (newData == true) {
+                  if(receivedChars[1] == 'A'){
+                      Lat = atof(receivedChars + 3);
+                  }
+                  else if(receivedChars[1] == 'O'){
+                      Lon = atof(receivedChars + 3);
+                  }
+                }
+                newData = false;
+                //strcpy(transmitChars, receivedChars);
+
   }
+ // Serial.print(Lat);
+ // Serial.print(Lon);
+                
+                
+  for (int i = 0; i < num_spoofers; i++) {
+    spoofers[i].updateLocation(Lat, Lon);
+  }
+  
   if(digitalRead(2) == 1){
   for (int i = 0; i < num_spoofers; i++) {
     spoofers[i].update();
